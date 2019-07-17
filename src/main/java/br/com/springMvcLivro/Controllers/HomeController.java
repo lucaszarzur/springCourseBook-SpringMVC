d52dce248dc2d81f6368dd5d1fbd8b73f5623a09
Lucas Zarzur <lucas.teles@fh.com.br>
2019-07-16 22:05:50 -0300

    Criação de estruturas e primeira controller

---
+++ src/main/java/br/com/springMvcLivro/Controllers/HomeController.java
@@ -0,0 +1,15 @@
+package br.com.springMvcLivro.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public void index() {
+        //aqui ainda vamos carregar os produtos.
+        System.out.println("Carregando os produtos");
+    }
+
+}
