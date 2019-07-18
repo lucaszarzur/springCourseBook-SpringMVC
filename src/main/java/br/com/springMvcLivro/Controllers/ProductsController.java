636f337978464ddc703e01fabc550fdfb982b9ca
Lucas Zarzur <lucas.teles@fh.com.br>
2019-07-17 21:17:07 -0300

    Included extra parameters in redirects

--- src/main/java/br/com/springMvcLivro/Controllers/ProductsController.java
+++ src/main/java/br/com/springMvcLivro/Controllers/ProductsController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @Transactional
@@ -19,24 +20,26 @@ public class ProductsController {
     @Autowired
     private ProductDAO productDAO;
 
-    @RequestMapping(method=RequestMethod.GET)
-    public ModelAndView list(){
-        ModelAndView modelAndView =
-                new ModelAndView("products/list");
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView list() {
+        ModelAndView modelAndView = new ModelAndView("products/list");
         modelAndView.addObject("products", productDAO.list());
+
         return modelAndView;
     }
 
     @RequestMapping(method=RequestMethod.POST)
-    public String save(Product product){
+    public String save(Product product, RedirectAttributes redirectAttributes) {
         productDAO.save(product);
-        System.out.println("Cadastrando o produto " + product);
-        return "products/ok";
+        redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso");
+
+        return "redirect:produtos";
     }
 
     @RequestMapping("/form")
-    public String form(Model model){
+    public String form(Model model) {
         model.addAttribute("types", Price.BookType.values());
+
         return "products/form";
     }
 }
