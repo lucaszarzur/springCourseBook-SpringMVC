1c43c5d7c1b96d820da5f852f867ae6ecc2d61d8
Lucas Zarzur <lucas.teles@fh.com.br>
2019-07-18 16:52:16 -0300

    Fix more logics

--- src/main/java/br/com/springMvcLivro/config/AppWebConfiguration.java
+++ src/main/java/br/com/springMvcLivro/config/AppWebConfiguration.java
@@ -6,6 +6,10 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.format.datetime.DateFormatterRegistrar;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.format.support.FormattingConversionService;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -26,6 +30,7 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".jsp");
+
         return resolver;
     }
 
@@ -37,6 +42,18 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
         bundle.setDefaultEncoding("UTF-8");
         // indica que o arquivo de mensagens deve ser recarregado a cada intervalo de tempo, no caso, foi configurado apenas um segundo
         bundle.setCacheSeconds(1);
+
         return bundle;
+    }
+
+    // formatador do Spring para data
+    @Bean
+    public FormattingConversionService mvcConversionService() {
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService(true);
+        DateFormatterRegistrar registrar = new DateFormatterRegistrar();
+        registrar.setFormatter(new DateFormatter("yyyy-MM-dd"));
+        registrar.registerFormatters(conversionService);
+
+        return conversionService;
     }
 }
--- src/main/java/br/com/springMvcLivro/models/Product.java
+++ src/main/java/br/com/springMvcLivro/models/Product.java
@@ -2,10 +2,12 @@ package br.com.springMvcLivro.models;
 
 
 import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.Min;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Entity
@@ -14,19 +16,21 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     @NotBlank
     private String title;
-
     @Lob
     @NotBlank
     private String description;
     @Min(30)
     private int pages;
+    @DateTimeFormat(iso= DateTimeFormat.ISO.DATE)
+    private Calendar releaseDate;
 
     @ElementCollection
     private List<Price> prices = new ArrayList<Price>();
 
+
+    // getters and setters
     public Integer getId() {
         return id;
     }
@@ -57,6 +61,22 @@ public class Product {
 
     public void setPages(int pages) {
         this.pages = pages;
+    }
+
+    public Calendar getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Calendar releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public List<Price> getPrices() {
+        return prices;
+    }
+
+    public void setPrices(List<Price> prices) {
+        this.prices = prices;
     }
 }
 
