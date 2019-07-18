9f1f7356abda5f755fd7bb4abec7377fe2c81297
Lucas Zarzur <lucas.teles@fh.com.br>
2019-07-17 21:44:51 -0300

    Fix code - Validator

--- src/main/java/br/com/springMvcLivro/Controllers/ProductsController.java
+++ src/main/java/br/com/springMvcLivro/Controllers/ProductsController.java
@@ -7,7 +7,6 @@ import br.com.springMvcLivro.validation.ProductValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
--- src/main/java/br/com/springMvcLivro/validation/ProductValidator.java
+++ src/main/java/br/com/springMvcLivro/validation/ProductValidator.java
@@ -7,12 +7,10 @@ import org.springframework.validation.Validator;
 
 public class ProductValidator implements Validator {
 
-    @Override
     public boolean supports(Class<?> clazz) {
         return Product.class.isAssignableFrom(clazz);
     }
 
-    @Override
     public void validate(Object target, Errors errors) {
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "title", "field.required");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "description", "field.required");
